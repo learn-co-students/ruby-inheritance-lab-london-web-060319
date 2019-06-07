4712564bfbec1fca058c3d4efc30a9724dfdb5c5
github username <github email address>
2019-06-07 09:27:17 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
-
+require_relative "./user.rb"
+class Student < User
+    def initialize
+        @knowledge = []
+    end
+  def learn(array) 
+    array = @knowledge
+    @knowledge << "Ruby framework Rails gem bundle update"
+  end
+  def knowledge
+    @knowledge << "Javascript Ember Elixir knowledge"
+  end
 end
